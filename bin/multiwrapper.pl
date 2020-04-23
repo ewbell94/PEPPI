15eebd01fb236d014402b702e9ef9c08b51274c0
Eric Bell <ewbell94@gmail.com>
2020-04-23 15:14:02 -0400

    TMSEARCH fully implemented

--- bin/multiwrapper.pl
+++ bin/multiwrapper.pl
@@ -9,5 +9,6 @@ my @interactions=split(",",$ARGV[1]);
 for my $int (@interactions){
     for my $prog (@supported){
 	`$int/*-$prog.pl 2> $int/err_$prog.log 1> $int/out_$prog.log`;
+	print `rm -rf $int/*-$prog.pl`;
     }
 }
